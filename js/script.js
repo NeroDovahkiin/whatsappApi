4ab43f2dcea1804c580405bb85d73b7f530da281
NeroDovahkiin <federixo_fgm@hotmail.com>
2022-03-09 12:25:41 -0300

    refactoring

--- js/script.js
+++ js/script.js
@@ -15,19 +15,19 @@ document.querySelector("#send_message").addEventListener("click", function () {
 // location message
 document.querySelector("#ubicacion").addEventListener("click", function () {
     let message = document.querySelector("#message");
-    let hora = new Date().getHours();
-    let saludo = () => {
-        if (hora >= 5 && hora < 12) {
+    let hour = new Date().getHours();
+    let greeting = () => {
+        if (hour >= 5 && hour < 12) {
             return "Buenos días!";
-        } else if (hora >= 12 && hora < 21) {
+        } else if (hour >= 12 && hour < 21) {
             return "Buenas tardes!";
         } else {
             return "buenas noches!";
         }
     };
 
-    message.innerText = ` ${saludo()} Aquí el auxilio mecánico...Puede enviar la ubicación por este medio. \n
-  Por favor enviar la ubicación que dice "UBICACIÓN ACTUAL", no la que
-   dice "en tiempo real"... Gracias!
+    message.innerText = ` ${greeting()} Aquí el auxilio mecánico...Puede enviar la ubicación por este medio. \n
+Por favor enviar la ubicación que dice "UBICACIÓN ACTUAL", no la que
+dice "en tiempo real"... Gracias!
   `;
 });
